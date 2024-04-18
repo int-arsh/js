4313613e7d6fefc589af4494a075ad5aa163ce5a
Akash Jain <jainaakash303@gmail.com>
2024-04-18 19:43:52 +0530

    forEach added

--- todo-list.js
+++ todo-list.js
@@ -4,12 +4,24 @@ renderTodoList();
 function renderTodoList(){
   let todoListHTML = '';
 
-  for (let i = 0;i<todoList.length; i++) {
+  todoList.forEach(function(todoObject, index) {
+    const { name,dueDate } = todoObject;
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate} </div>   
+      <button onclick="
+        todoList.splice(${index},1);
+        renderTodoList();
+      "class="delete-todo-button">Delete</button>`;
+    todoListHTML += html;
+  });
+
+
+  /*for (let i = 0;i<todoList.length; i++) {
     const todoObject = todoList[i];
     // const name = todoObject.name;
     // const dueDate  = todoObject.dueDate;
     const { name,dueDate } = todoObject;
-
     const html = `
       <div>${name}</div>
       <div>${dueDate} </div>   
@@ -17,9 +29,10 @@ function renderTodoList(){
         todoObject.splice(${i},1);
         renderTodoList();
       "class="delete-todo-button">Delete</button>`;
-
     todoListHTML += html;
   }
+  */
+
 
   document.querySelector('.js-todo-list')
   .innerHTML = todoListHTML;
